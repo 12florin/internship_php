6476ec92bef83ef0142771b7c64cdb6f1bbcaee1
12florin <f.toader94@yahoo.com>
2021-08-10 13:32:35 +0300

    CMS-Profile

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -5,15 +5,17 @@
 <?php ob_start(); ?>
 <?php session_start(); ?>
 
-
-
 <?php      
 
 if(!isset($_SESSION['user_role'])){
     
  header("Location: ../index.php");
 
- }
+ }else {
+     if($_SESSION['user_role'] !='admin'){
+        header("Location: ../index.php");
+     }
+    }
 
 
 ?>
