913932995422b40fb21fd9a9fa74b00338a2b710
12florin <f.toader94@yahoo.com>
2021-08-10 13:47:56 +0300

    CMS-Dashboard

--- admin/index.php
+++ admin/index.php
@@ -14,20 +14,122 @@ if ($connection) echo "Connection done";
 
     <div class="container-fluid">
 
-        <!-- Page Heading -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">
-                    Welcome to admin
+<!-- Page Heading -->
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">
+            Welcome to admin
+            <small>
+    <?php  echo $_SESSION['username'];
+            ?></small>
+        </h1>
+        
+    </div>
+</div>
 
-                    <small>
-          <?php  echo $_SESSION['username'];
-                    ?></small>
-                </h1>
+<!-- /.row -->
+       
+                <!-- /.row -->
                 
+                <div class="row">
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-file-text fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <?php
+                        
+                        $query = "SELECT * FROM posts";
+                        $select_all_posts = mysqli_query($connection,$query);
+                        $post_counts = mysqli_num_rows($select_all_posts);
+                       // echo "<div class='huge'>{$post_counts}</div"
+                        ?>
+                  <div class='huge'><?php echo $post_counts;  ?></div>
+                        <div>Posts</div>
+                    </div>
+                </div>
             </div>
+            <a href="posts.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
         </div>
-        <!-- /.row -->
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-green">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-comments fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                     <div class='huge'>23</div>
+                      <div>Comments</div>
+                    </div>
+                </div>
+            </div>
+            <a href="comments.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-yellow">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-user fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                    <div class='huge'>23</div>
+                        <div> Users</div>
+                    </div>
+                </div>
+            </div>
+            <a href="users.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-red">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-list fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div class='huge'>13</div>
+                         <div>Categories</div>
+                    </div>
+                </div>
+            </div>
+            <a href="categories.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+                <!-- /.row -->
+
 
     </div>
     <!-- /.container-fluid -->
