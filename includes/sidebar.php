d908df20140eb518b261cb5df0dad61501ceaf75
12florin <f.toader94@yahoo.com>
2021-08-10 11:30:32 +0300

    CMS-Login

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -9,7 +9,7 @@
 
 <div class="well">
     <h4>Blog Search</h4>
-    <form action="" method = "post">
+    <form action="search.php" method = "post">
     <div class="input-group">
         <input name ="search" type="text" class="form-control">
         <span class="input-group-btn">
@@ -19,13 +19,31 @@
         </span>
     </div>
     </form> <!--search form-->
-
-
-
-
     <!-- /.input-group -->
 </div>
 
+
+<!-- Login -->
+
+<div class="well">
+    <h4>Login</h4>
+    <form action="includes/login.php" method = "post">
+    <div class="form-group">
+     <input name ="username" type="text" class="form-control" placeholder= "Enter username">
+ </div>
+
+ <div class="input-group">
+     <input name ="password" type="password" class="form-control" placeholder= "Enter password">
+     <span class = "input-group-btn">
+        <button class = "btn btn-primary" name="login" type ="submit"> 
+            <span>Submit</span>
+        </button>
+     </span>
+ </div>
+
+</form> <!--search form-->
+<!-- /.input-group -->
+</div>
 
 
 
@@ -33,7 +51,6 @@
 
 <!-- Blog Categories Well -->
 <div class="well">
-
 <?php 
 $query = "SELECT * FROM categories  ";
 $select_categories_sidebar = mysqli_query($connection,$query);
@@ -46,14 +63,11 @@ $select_categories_sidebar = mysqli_query($connection,$query);
             
             <?php
             while($row = mysqli_fetch_assoc($select_categories_sidebar)){
-
-           $cat_title =  $row['cat_title'];
-           $cat_id =  $row['cat_id'];
+             $cat_title =  $row['cat_title'];
+             $cat_id =  $row['cat_id'];
            echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
             }
-           ?>
-
-                
+           ?>  
             </ul>
         </div>
 
